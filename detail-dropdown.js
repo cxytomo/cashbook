436d2952f27309bc1f9eecaae967b25c193f9ba3
tomo <cxytomo@gmail.com>
2012-11-09 11:07:56 +0800

    create-more menu comleted
    this edition uses raw js,try to use jQuery for convenience.

--- detail-dropdown.js
+++ detail-dropdown.js
@@ -1,4 +1,4 @@
-var daydetail = document.getElementsByClassName('day-detail');
+var daydetail = document.getElementsByClassName('day_detail');
 var dropdown = document.getElementsByClassName('daytotal');
 /*
 1.daydetail[0]换成普遍方法
@@ -11,29 +11,26 @@ var show = function (e) {
 	targ = e.target || e.srcElement;
 	if(targ.className.search('daytotal') >= 0){
 		wrap = targ.parentNode.parentNode;
-		current = wrap.getElementsByClassName('day-detail')[0];
-		status = window.getComputedStyle(current,"").getPropertyValue('display');
+		current = wrap.getElementsByClassName('day_detail')[0];
+		status = window.getComputedStyle(current,'').getPropertyValue('display');
 		prev_sibling = wrap.previousElementSibling;//IE not support
 		next_sibling = wrap.nextElementSibling;
 		if(status === "none") {
 			current.style.display = "block";
 			wrap.style.margin = "5px 0";
-			wrap.getElementsByClassName('day-total')[0].style.borderTop = "1px solid #d8d8d8";
-
-			if(next_sibling) {	 			                        
-				next_sibling.getElementsByClassName('day-total')[0].style.borderTop = "1px solid #d8d8d8";	
-			}
-			
+			wrap.getElementsByClassName('day_total')[0].style.borderTop = "1px solid #d8d8d8";
+			if(next_sibling) {
+				next_sibling.getElementsByClassName('day_total')[0].style.borderTop = "1px solid #d8d8d8";	
+			}	
 		} 
 		else if(status === "block") {
 			current.style.display = "none";
 			wrap.style.margin = "0 0";
 			if(window.getComputedStyle(prev_sibling,"").getPropertyValue('margin') === "0px") {
-				wrap.getElementsByClassName('day-total')[0].style.borderTop = "none";
+				wrap.getElementsByClassName('day_total')[0].style.borderTop = "none";
 			}
 			if(next_sibling && window.getComputedStyle(next_sibling,"").getPropertyValue('margin') === "0px") {
-			console.log(next_sibling);
-			next_sibling.getElementsByClassName('day-total')[0].style.borderTop = "none";
+			next_sibling.getElementsByClassName('day_total')[0].style.borderTop = "none";
 			}
 //			targ.style.backgroundPosition = "0 0";
 			
@@ -56,16 +53,23 @@ else if(document.attachEvent){
 */
 
 if(document.addEventListener){
-	document.getElementById('data-table').addEventListener("click",show,false);
+	document.getElementById('data_table').addEventListener("click",show,false);
 } 
 else if(document.attachEvent){
-	document.getElementById('data-table').attachEvent("onclick",show);
+	document.getElementById('data_table').attachEvent("onclick",show);
 }
 
 var addmore = function (e) {
+	var add_form,status;
 	e = e || window.event;
-	var add_form = document.getElementsByClassName('form-add-more')[0];
-	add_form.style.display = "block";
+	add_form = document.getElementsByClassName('form_add_more')[0];
+	status = window.getComputedStyle(add_form,'').getPropertyValue('display');
+	if (status === 'none') {
+		add_form.style.display = 'block';
+	}
+	else if (status === 'block') {
+		add_form.style.display = 'none';
+	}
 	e.preventDefault();
 }
-document.getElementsByClassName('create-more')[0].addEventListener('click',addmore,false);
+document.getElementsByClassName('create_more')[0].addEventListener('click',addmore,false);
