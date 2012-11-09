b130d5108f204082148235d2acd4c9c4bdcf8b0a
tomo <cxytomo@gmail.com>
2012-11-09 15:04:04 +0800

    freely star or unstar
    By changing the className of the element,change the status of the
    element,ie,if it is starred ,click to unstar it ,vice versa.

--- detail-dropdown.js
+++ detail-dropdown.js
@@ -5,61 +5,39 @@ var dropdown = document.getElementsByClassName('daytotal');
 2.试试body上监听事件发生,只点a标签才触发
 3.等等
 */
-var show = function (e) {	
+$('#data_table').click(function (e) {	
 	var targ,wrap,current,status,prev_sibling,next_sibling;
 	e = e || window.event;
 	targ = e.target || e.srcElement;
-	if(targ.className.search('daytotal') >= 0){
+	if (targ.className.search('daytotal') >= 0){
 		wrap = targ.parentNode.parentNode;
 		current = wrap.getElementsByClassName('day_detail')[0];
 		status = window.getComputedStyle(current,'').getPropertyValue('display');
 		prev_sibling = wrap.previousElementSibling;//IE not support
 		next_sibling = wrap.nextElementSibling;
-		if(status === "none") {
+		if (status === "none") {
 			current.style.display = "block";
 			wrap.style.margin = "5px 0";
 			wrap.getElementsByClassName('day_total')[0].style.borderTop = "1px solid #d8d8d8";
-			if(next_sibling) {
+			if (next_sibling) {
 				next_sibling.getElementsByClassName('day_total')[0].style.borderTop = "1px solid #d8d8d8";	
 			}	
 		} 
-		else if(status === "block") {
+		else if (status === "block") {
 			current.style.display = "none";
 			wrap.style.margin = "0 0";
-			if(window.getComputedStyle(prev_sibling,"").getPropertyValue('margin') === "0px") {
+			if (window.getComputedStyle(prev_sibling,"").getPropertyValue('margin') === "0px") {
 				wrap.getElementsByClassName('day_total')[0].style.borderTop = "none";
 			}
-			if(next_sibling && window.getComputedStyle(next_sibling,"").getPropertyValue('margin') === "0px") {
+			if (next_sibling && window.getComputedStyle(next_sibling,"").getPropertyValue('margin') === "0px") {
 			next_sibling.getElementsByClassName('day_total')[0].style.borderTop = "none";
 			}
-//			targ.style.backgroundPosition = "0 0";
-			
 		}
 		e.preventDefault();
 	}	
-};
+});
 
-/*
-if(document.addEventListener){
-	for(var i = 0;i < dropdown.length; i++) { 
-		dropdown[i].addEventListener("click", show, false);
-	}
-}
-else if(document.attachEvent){
-	for(var i = 0;i < dropdown.length; i++) { 
-		dropdown[i].attachEvent("onclick", show, false);
-	}
-}
-*/
-
-if(document.addEventListener){
-	document.getElementById('data_table').addEventListener("click",show,false);
-} 
-else if(document.attachEvent){
-	document.getElementById('data_table').attachEvent("onclick",show);
-}
-
-var addmore = function (e) {
+$('.create_more').click(function (e) {
 	var add_form,status;
 	e = e || window.event;
 	add_form = document.getElementsByClassName('form_add_more')[0];
@@ -71,5 +49,19 @@ var addmore = function (e) {
 		add_form.style.display = 'none';
 	}
 	e.preventDefault();
-}
-document.getElementsByClassName('create_more')[0].addEventListener('click',addmore,false);
+});
+
+$('#data_table .tag a').click(function (e) {
+	var targ,status;
+	e = e || window.event;
+	targ = e.target || e.srcElement;
+	if (targ.className.search('unstar') >= 0) {
+		status = targ.className.replace('unstar', 'star');
+		targ.className = status;
+	}
+	else {
+		status = targ.className.replace('star', 'unstar');
+		targ.className = status;
+	}
+	e.preventDefault();
+});
